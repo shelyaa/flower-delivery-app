50ce2c409948b3a4cea88a1a333f33dda02d90b5
Aliona <ashelyaa17@gmail.com>
2025-09-15 19:10:19 +0300

    fix

---
+++ backend/api/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const sequelize = require("../src/db");
+const shopRouter = require("../src/routes/shop.router");
+const flowerRouter = require("../src/routes/flower.router");
+const orderRouter = require("../src/routes/order.router");
+
+const app = express();
+app.use(cors({
+  origin: [
+    "https://flower-delivery-app-shrl.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true,
+}));
+
+app.use(express.json());
+
+app.use("/shops", shopRouter);
+app.use("/flowers", flowerRouter);
+app.use("/orders", orderRouter);
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Database connection failed:", err));
+
+
+
+module.exports = (req, res) => app(req, res); 
